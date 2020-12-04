a9b17406a86ab4507b431602567bddc246f15721
Ben Gunn <ben.m.gunn@gmail.com>
2020-12-04 17:11:42 +1100

    Initial commit

---
+++ Example.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\example\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'Example' block.
+ *
+ * @Block(
+ *  id = "example_block",
+ *  admin_label = @Translation("Example Block"),
+ * )
+ */
+class Example extends BlockBase implements ContainerFactoryPluginInterface {
+  protected $formBuilder;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $form = $this->formBuilder->getForm('Drupal\example\Form\ExampleForm');
+    return $form;
+  }
+
+}
